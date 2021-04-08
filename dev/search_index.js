ea92471396d4fa273e3e35dbc9abb8d99d00c6f6
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 11:31:15 +0000

    build based on 95c9d6c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProtoMarketData","category":"page"},{"location":"#ProtoMarketData","page":"Home","title":"ProtoMarketData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProtoMarketData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProtoMarketData]","category":"page"}]
+}
